ac0dc9b384de058ec2fee2104d55250c069d05a1
kamillamamatova <kamillamamatova.km@gmail.com>
2026-03-14 21:10:09 -0400

    Chnaged from port 5000 to 5001

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) =>
   res.send('API is running');
 });
 
-app.listen(5000, () =>
+app.listen(5001, () =>
 {
-  console.log('Server started on port 5000');
+  console.log('Server started on port 5001');
 });
