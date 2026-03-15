aede90a5a3f95fcd575d2ef3afb5861f81681841
kamillamamatova <kamillamamatova.km@gmail.com>
2026-03-14 21:13:34 -0400

    Added API without database

--- server.js
+++ server.js
@@ -6,10 +6,163 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Basic test route
-app.get('/', (req, res) =>
+let cardList =
+[
+  'Roy Campanella',
+  'Paul Molitor',
+  'Tony Gwynn',
+  'Dennis Eckersley',
+  'Reggie Jackson',
+  'Galord Perry',
+  'Buck Leonard',
+  'Rollie Fingers',
+  'Charlie Gehringer',
+  'Wade Boggs',
+  'Carl Hubbell',
+  'Dave Winfield',
+  'Jackie Robinson',
+  'Ken Griffey, Jr.',
+  'Al Simmons',
+  'Chuck Klein',
+  'Mel Ott',
+  'Mark McGwire',
+  'Nolan Ryan',
+  'Ralph Kiner',
+  'Yogi Berra',
+  'Goose Goslin',
+  'Greg Maddux',
+  'Frankie Frisch',
+  'Ernie Banks',
+  'Ozzie Smith',
+  'Hank Greenberg',
+  'Kirby Puckett',
+  'Bob Feller',
+  'Dizzy Dean',
+  'Joe Jackson',
+  'Sam Crawford',
+  'Barry Bonds',
+  'Duke Snider',
+  'George Sisler',
+  'Ed Walsh',
+  'Tom Seaver',
+  'Willie Stargell',
+  'Bob Gibson',
+  'Brooks Robinson',
+  'Steve Carlton',
+  'Joe Medwick',
+  'Nap Lajoie',
+  'Cal Ripken, Jr.',
+  'Mike Schmidt',
+  'Eddie Murray',
+  'Tris Speaker',
+  'Al Kaline',
+  'Sandy Koufax',
+  'Willie Keeler',
+  'Pete Rose',
+  'Robin Roberts',
+  'Eddie Collins',
+  'Lefty Gomez',
+  'Lefty Grove',
+  'Carl Yastrzemski',
+  'Frank Robinson',
+  'Juan Marichal',
+  'Warren Spahn',
+  'Pie Traynor',
+  'Roberto Clemente',
+  'Harmon Killebrew',
+  'Satchel Paige',
+  'Eddie Plank',
+  'Josh Gibson',
+  'Oscar Charleston',
+  'Mickey Mantle',
+  'Cool Papa Bell',
+  'Johnny Bench',
+  'Mickey Cochrane',
+  'Jimmie Foxx',
+  'Jim Palmer',
+  'Cy Young',
+  'Eddie Mathews',
+  'Honus Wagner',
+  'Paul Waner',
+  'Grover Alexander',
+  'Rod Carew',
+  'Joe DiMaggio',
+  'Joe Morgan',
+  'Stan Musial',
+  'Bill Terry',
+  'Rogers Hornsby',
+  'Lou Brock',
+  'Ted Williams',
+  'Bill Dickey',
+  'Christy Mathewson',
+  'Willie McCovey',
+  'Lou Gehrig',
+  'George Brett',
+  'Hank Aaron',
+  'Harry Heilmann',
+  'Walter Johnson',
+  'Roger Clemens',
+  'Ty Cobb',
+  'Whitey Ford',
+  'Willie Mays',
+  'Rickey Henderson',
+  'Babe Ruth'
+];
+
+app.post('/api/addcard', async (req, res) =>
 {
-  res.send('API is running');
+  let error = '';
+  const { userId, card } = req.body;
+
+  cardList.push(card);
+
+  const ret = { error: error };
+  res.status(200).json(ret);
+});
+
+app.post('/api/login', async (req, res) =>
+{
+  let error = '';
+
+  const { login, password } = req.body;
+
+  let id = -1;
+  let fn = '';
+  let ln = '';
+
+  if (login.toLowerCase() === 'rickl' && password === 'COP4331')
+  {
+    id = 1;
+    fn = 'Rick';
+    ln = 'Leinecker';
+  }
+  else
+  {
+    error = 'Invalid user name/password';
+  }
+
+  const ret = { id: id, firstName: fn, lastName: ln, error: error };
+  res.status(200).json(ret);
+});
+
+app.post('/api/searchcards', async (req, res) =>
+{
+  const { userId, search } = req.body;
+
+  const _search = search.toLowerCase().trim();
+  const _ret = [];
+
+  for (let i = 0; i < cardList.length; i++)
+  {
+    const lowerFromList = cardList[i].toLowerCase();
+    if (lowerFromList.indexOf(_search) >= 0)
+    {
+      _ret.push(cardList[i]);
+    }
+  }
+
+  const ret = { results: _ret, error: '' };
+  res.status(200).json(ret);
 });
 
 app.listen(5001, () =>
