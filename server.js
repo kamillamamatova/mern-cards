4d6268112095e1013f32244d37a140655042c29d
kamillamamatova <kamillamamatova.km@gmail.com>
2026-03-14 20:53:01 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Basic test route
+app.get('/', (req, res) =>
+{
+  res.send('API is running');
+});
+
+app.listen(5000, () =>
+{
+  console.log('Server started on port 5000');
+});
